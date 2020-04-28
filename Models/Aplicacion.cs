edd49952b1bc729d258821318ca391664881b362
chekulhanseim <48593404+chekulhanseim@users.noreply.github.com>
2020-04-28 20:55:41 +0200

    Inicial Files

---
+++ Models/Aplicacion.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JSONReader.Models
+{
+    public class Aplicacion
+    {
+        public string Nombre { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using JSONReader.Models;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JSONReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenidos!");
+
+            var jsonString = File.ReadAllText("config.json");
+           
+            var appModel = JsonSerializer.Deserialize<Aplicacion>(jsonString);
+
+            Console.WriteLine("Has iniciado la aplicacion con las siguientes parametros:");
+            Console.WriteLine("NOMBRE: {0}, YEAR: {1} ", appModel.Nombre, appModel.Year);
+
+        }
+    }
+}
